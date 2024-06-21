35c7d1f8527a28355ccdd663a5f907e17f027540
Anurag Bhandary <anuragb2901@gmail.com>
2024-06-21 14:11:17 -0500

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "sunflower" : "https://www.youtube.com/watch?v=r7Rn4ryE_w8",
+    "sugar" : "https://www.youtube.com/watch?v=iPcX6EpJI3o",
+    "lover" : "https://www.youtube.com/watch?v=mH_LFkWxpI0",
+    "bang" : "https://www.youtube.com/watch?v=jZyAB2KFDls",
+    "tere" : "https://www.youtube.com/watch?v=KUpwupYj_tY",
+    "ghungroo" : "https://www.youtube.com/watch?v=qFkNATtc3mc",
+    "dilbar" : "https://www.youtube.com/watch?v=YnreYUTRgBE"
+}
